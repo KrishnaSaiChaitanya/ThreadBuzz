b518905458296817b36f2da5e898d5b1fe9025d9
KrishnaSaiChaitanya <chaitanya.tata215@gmail.com>
2024-10-14 10:54:43 +0000

    feat: update nav banner

--- src/components/threads-banner.tsx
+++ src/components/threads-banner.tsx
@@ -1,21 +1,25 @@
-"use client"
+"use client";
 
-import useWindow from '@/hooks/use-window'
-import React from 'react'
-import { Icons } from '@/components/icons'
-import Image from 'next/image'
+import useWindow from "@/hooks/use-window";
+import React from "react";
+import { Icons } from "@/components/icons";
+import Image from "next/image";
 
 export default function ThreadsBanner() {
-    const { isMobile } = useWindow()
-    return (
-        <header className="max-w-screen-md  md:max-w-screen-2xl lg:max-w-[1800px] mx-auto">
-            {isMobile ?
-                <div className='flex justify-center items-center '>
-                    <Icons.logo className='h-10 w-10 mb-6 sm:h-16 sm:w-16 mt-16' />
-                </div>
-                :
-                <nav className='flex w-full justify-between items-center z-50 pointer-events-none select-none'>
-                    <Image
+  const { isMobile } = useWindow();
+  return (
+    <header className="mx-auto  max-w-screen-md md:max-w-screen-2xl lg:max-w-[1800px]">
+      {isMobile ? (
+        <div className="flex items-center justify-center ">
+          <Icons.logo className="mb-6 mt-16 h-10 w-10 sm:h-16 sm:w-16" />
+        </div>
+      ) : (
+        <nav className="pointer-events-none z-50 flex w-full select-none items-center justify-center">
+          <Icons.logo className="mb-6 mt-16 h-8 w-8 sm:h-16 sm:w-16" />
+          <h2 className=" mb-6 ml-5 mt-16 text-[28px] font-bold text-white ">
+            Thread Buzz
+          </h2>
+          {/* <Image
                         width={1000}
                         height={1000}
                         src="/bg.webp"
@@ -23,10 +27,9 @@ export default function ThreadsBanner() {
                         className="w-full h-[500px] object-cover"
                         unoptimized
                         priority
-                    />
-                </nav>
-            }
-        </header>
-    )
+                    /> */}
+        </nav>
+      )}
+    </header>
+  );
 }
-
