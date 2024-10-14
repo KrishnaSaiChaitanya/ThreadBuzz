3965079dd1f7b458ddcc12f1b69e0f49dc54af92
KrishnaSaiChaitanya <chaitanya.tata215@gmail.com>
2024-10-14 10:53:49 +0000

    feat: update root loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,31 +1,25 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 export default function RootLoading() {
-    const LogoImage = '/logo.png';
-    const MetaImage = '/meta.png';
+  const LogoImage = "/logo.png";
+  const MetaImage = "/meta.png";
 
-    return (
-        <div className="flex flex-col justify-between h-screen ">
-            <div className="flex-grow flex items-center justify-center ">
-                <Image
+  return (
+    <div className="flex h-screen flex-col justify-between ">
+      <div className="flex flex-grow items-center justify-center ">
+        {/* <Image
                     alt="Threads"
                     src={LogoImage}
                     width={100}
                     height={100}
                     priority
-                />
-            </div>
-            <div className='w-full  flex justify-center items-center mb-6'>
-                <Image
-                    priority
-                    alt="Meta"
-                    src={MetaImage}
-                    width={90}
-                    height={90}
-                    className='w-auto'
-                />
-            </div>
-        </div>
-    );
+                /> */}
+        <p>Loading ...</p>
+      </div>
+      <div className="mb-6  flex w-full items-center justify-center">
+        <h2>Made BY T.K.S Chaitanya</h2>
+      </div>
+    </div>
+  );
 }
