72f825fd162050373ad4478d08932dd930bceba2
KrishnaSaiChaitanya <chaitanya.tata215@gmail.com>
2024-10-14 10:52:43 +0000

    chore: meta data changes

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Threads",
-    description: "Open source threads clone with t3 stack.",
-    url: "https://threads.codebustar.com.vercel.app",
-    ogImage: "https://threads.codebustar.com/opengraph-image.png",
-    links: {
-        twitter: "https://twitter.com/sujjeeee",
-        github: "https://github.com/sujjeee",
-        discord: "https://discord.com/users/sujjeee",
-    },
-}
+  name: "ThreadBuzz",
+  description: "",
+  url: "https://thread-buzz.vercel.app/",
+  ogImage: "https://threads.codebustar.com/opengraph-image.png",
+  links: {
+    twitter: "",
+    github: "https://github.com/KrishnaSaiChaitanya",
+    discord: "",
+  },
+};
